38a9e3b33f129793ae2359d08d0a9fb5e622b476
Elisabeth <Elisabeth@CFIAMLA2>
2017-01-29 15:55:04 +0100

    PP Sunday programming

---
+++ src/java/student/NavigationController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Elisabeth
+ */
+@Named(value = "navigationController")
+@RequestScoped
+public class NavigationController {
+
+        public String navigate() {
+        String navAction = "confirmation";
+        return navAction;
+    }
+
+    
+    /**
+     * Creates a new instance of NavigationController
+     */
+    public NavigationController() {
+    }
+    
+}
